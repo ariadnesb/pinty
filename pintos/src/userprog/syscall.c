c5f70a345cd0f53c7105a6b9550d0c82c5e2c5c1
ariadnesb <ariadne.sinnisbourozikas@gmail.com>
2018-04-20 16:13:30 -0400

    setting up to start writing our syscalls

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -15,6 +15,34 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
-  printf ("system call!\n");
-  thread_exit ();
+	int call = *(* int) f-> esp;
+
+	switch(call){
+		case  SYS_HALT:                   /* Halt the operating system. */
+    	case SYS_EXIT:                  /* Terminate this process. */
+    	case SYS_EXEC:                   /* Start another process. */
+    	case SYS_WAIT:                   /* Wait for a child process to die. */
+   		case SYS_CREATE:                 /* Create a file. */
+    	case SYS_REMOVE:                 /* Delete a file. */
+    	case SYS_OPEN:                   /* Open a file. */
+    	case SYS_FILESIZE:               /* Obtain a file's size. */
+    	case SYS_READ:                   /* Read from a file. */
+    	case SYS_WRITE:                  /* Write to a file. */
+    	case SYS_SEEK:                   /* Change position in a file. */
+    	case SYS_TELL:                   /* Report current position in a file. */
+    	case SYS_CLOSE:                  /* Close a file. */
+
+    /* Project 3 and optionally project 4. */
+    	case SYS_MMAP:                   /* Map a file into memory. */
+    	case SYS_MUNMAP:                 /* Remove a memory mapping. */
+
+		default:
+			printf ("system call!\n");
+  			thread_exit ();
+
+
+
+	}
+
+  
 }
